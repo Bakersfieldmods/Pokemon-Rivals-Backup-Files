c83fba8859e1fbfda0dde4ec1e8bad5205dfa131
Bakersfieldmods <34212182+Bakersfieldmods@users.noreply.github.com>
2018-05-28 08:20:01 -0500

    Initial push

---
+++ Pokemon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="_free__new_tiles_mastersheet_by_magiscarf-daq6lcu" tilewidth="16" tileheight="16" tilecount="11988" columns="108">
+ <image source="../../Downloads/_free__new_tiles_mastersheet_by_magiscarf-daq6lcu.png" trans="e5ece8" width="1728" height="1776"/>
+</tileset>
